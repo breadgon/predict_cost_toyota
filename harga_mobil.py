224d9a58139b11081d06ea61a173f6fd2fd4e587
Rangga <rangga@Ranggas-MacBook-Air.local>
2023-10-12 16:51:04 +0700

    change deploy

--- harga_mobil.py
+++ harga_mobil.py
@@ -7,7 +7,7 @@ trained_model = load("Model_toyota.pkl")
 
 form = st.form(key='my-form')
 
-models = st.sidebar.selectbox("Masukkan model: ", ('GT86', 'Corolla', 'RAV4', 'Yaris', 'Auris', 'Aygo', 'C-HR',
+models = st.sidebar.selectbox("Car Model: ", ('GT86', 'Corolla', 'RAV4', 'Yaris', 'Auris', 'Aygo', 'C-HR',
        'Prius', 'Avensis', 'Verso', 'Hilux', 'PROACE VERSO', 'Land Cruiser', 'Supra', 'Camry', 'Verso-S', 'IQ',
        'Urban Cruiser'))
 Model = {'GT86': 6, 'Corolla': 5, 'RAV4': 12, 'Yaris': 17, 'Auris': 0, 'Aygo': 2, 'C-HR': 3,
@@ -16,29 +16,29 @@ Model = {'GT86': 6, 'Corolla': 5, 'RAV4': 12, 'Yaris': 17, 'Auris': 0, 'Aygo': 2
 if models:
   model = Model[models]
 
-years = st.slider("Masukkan tahun: ", 1998, 2020)
+years = st.slider("Model Year: ", 1998, 2024)
 Year = {'1998': 0, '1999': 1, '2000': 2, '2001': 3, '2002': 4, '2003': 5, '2004': 6, '2005': 7, '2006': 8, '2007': 9, 
         '2008': 10, '2009': 11, '2010': 12, '2011': 13, '2012': 14, '2013': 15, '2014': 16, '2015': 17, '2016': 18, '2017': 19,
-        '2018': 20, '2019': 21, '2020': 22}
+        '2018': 20, '2019': 21, '2020': 22, '2021': 23, '2022': 24, '2023': 25, '2024': 26}
 if years:
   year = Year[str(years)]
 
-Trans = st.sidebar.selectbox("Masukkan transmission: ", ('manual', 'automatic', 'semi-auto', 'other'))
+Trans = st.sidebar.selectbox("Enter Transmission: ", ('manual', 'automatic', 'semi-auto', 'other'))
 transmission = {'manual': 1, 'automatic': 0, 'semi-auto': 3, 'other': 2}
 if Trans:
   trans = transmission[Trans]
 
-Fuel = st.sidebar.selectbox("Masukkan fuelType: ", ('Petrol', 'Other', 'Hybrid', 'Diesel'))
+Fuel = st.sidebar.selectbox("Enter FuelType: ", ('Petrol', 'Other', 'Hybrid', 'Diesel'))
 fuelType = {'Petrol': 3, 'Other': 2, 'Hybrid': 1, 'Diesel': 0}
 if Fuel:
   fuel = fuelType[Fuel]
 
-mile = form.text_input("Masukkan mileage (Km): ")
-tax = form.text_input("Masukkan tax: ")
-mpg = st.slider("Masukkan mpg (litres): ", 2.8, 235.0)
-engSize = st.slider("Masukkan engineSize (litre): ", 0.0, 4.5)
+mile = form.text_input("Enter MileAge (Km): ")
+tax = form.text_input("Enter Tax ($): ")
+mpg = st.slider("Enter Miles per Gallon (litres): ", 2.8, 235.0)
+engSize = st.slider("Enter EngineSize (litre): ", 0.0, 4.5)
 
-submit = form.form_submit_button("Cek Harga")
+submit = form.form_submit_button("Check Price")
 
 if submit:
   hasil_akhir = trained_model.predict([[
@@ -50,8 +50,8 @@ if submit:
     int(tax),
     float(mpg), 
     float(engSize)]])
-  st.write("Kategori Harga:")
-  st.write(hasil_akhir[0])
+  st.write("Price Category:")
+  st.write(f"${hasil_akhir[0]}")
 
 # ngrok_tunnel = ngrok.connect(8501)
 # print('Public URL:', ngrok_tunnel.public_url)
